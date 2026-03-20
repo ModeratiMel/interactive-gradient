44688ece666c9aa10af8e373c27ef9aae36a397d
Melissa Gabriele <110409921+ModeratiMel@users.noreply.github.com>
2026-03-20 15:13:35 -0600

    feat: remove unneeded from home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,28 @@
-import { useEffect, useState, useRef } from "react";
-import PhysicsExp from "../components/threejs/PhysicsExp";
-import Portal from "../components/threejs/Portal";
+import { useEffect, useState } from "react";
 import { Canvas } from '@react-three/fiber'
-import GalaxyExp from "../components/threejs/GalaxyExp";
-import Waves from "../components/threejs/Waves";
-import Grain from "../components/threejs/Grain";
 import MovingGradient from "../components/threejs/MovingGradient";
 import { Leva } from "leva";
 
 export default function Home()
 {
 
-    // //mouse position
-    // const [mousePosition,setMousePosition] = useState({ x: 1, y: 1 });
-    // const updateMousePosition = ev => {
-    //     let fromCenterX = ev.clientX/window.innerWidth
-    //     let fromCenterY = ev.clientY/window.innerHeight
-    //     setMousePosition({ x: fromCenterX, y: fromCenterY });
-    // };
-    // useEffect(()=> {
-    //     window.addEventListener('mousemove', updateMousePosition);
-    // })
+    //mouse position
+    const [mousePosition,setMousePosition] = useState({ x: 1, y: 1 });
+    const updateMousePosition = ev => {
+        let fromCenterX = ev.clientX/window.innerWidth
+        let fromCenterY = ev.clientY/window.innerHeight
+        setMousePosition({ x: fromCenterX, y: fromCenterY });
+    };
+    useEffect(()=> {
+        window.addEventListener('mousemove', updateMousePosition);
+    })
 
-    // //window size
-    // const [windowSize, setWindowSize] = useState({x:window.innerWidth, y:window.innerHeight})
-    // const updateSize = () => {
-    //     setWindowSize({x:window.innerWidth, y:window.innerHeight})
-    // }
-    // window.addEventListener('resize', updateSize)
+    //window size
+    const [windowSize, setWindowSize] = useState({x:window.innerWidth, y:window.innerHeight})
+    const updateSize = () => {
+        setWindowSize({x:window.innerWidth, y:window.innerHeight})
+    }
+    window.addEventListener('resize', updateSize)
 
     return <>
         {/* <div className="absolute top-0 left-0 w-screen h-screen z-10">
@@ -41,13 +36,12 @@ export default function Home()
         <div className="absolute top-0 left-0 w-screen h-screen z-0">
             <Leva/>
             <Canvas frameloop="always">
-                {/* <Portal 
+                <MovingGradient
                     mousePosition={mousePosition}
                     windowSize={windowSize}
-                />  */}
-                <MovingGradient/>
+                />
             </Canvas>
         </div>
-        <div className="w-full h-full bg-black"></div>
+        {/* <div className="w-full h-full bg-red-100"></div> */}
     </>
 }
